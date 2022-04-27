8620a64b6bba8e7eab9e09620d7eaaef1db91ea6
cesartidrick <cesartidrick@gmail.com>
2022-04-27 09:27:17 -0600

    commit tree 7cb005af7e9defac1bf5c6db692175f381ad5642

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(name) {
+    return console.log(name.toUpperCase());
+}
+console.log(upperCaseCustomerName(customerName));
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob'
+    return console.log(bestCustomer);
+}
+setBestCustomer();
